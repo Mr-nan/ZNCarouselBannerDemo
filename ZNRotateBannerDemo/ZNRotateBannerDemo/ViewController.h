0c4314225fd6bafe83c989b8f47d193c3624d928
zhengnan <zhangnan@chedai.com>
2016-07-05 14:57:38 +0800

    createFile

---
+++ ZNRotateBannerDemo/ZNRotateBannerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZNRotateBannerDemo
+//
+//  Created by 郑南 on 16/7/5.
+//  Copyright © 2016年 ZN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZNRotateBannerDemo/ZNRotateBannerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZNRotateBannerDemo
+//
+//  Created by 郑南 on 16/7/5.
+//  Copyright © 2016年 ZN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
