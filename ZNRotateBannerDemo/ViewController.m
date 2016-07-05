6bc871497992a18861eec97da3ed704fac8c48cf
zhengnan <zhangnan@chedai.com>
2016-07-05 15:22:28 +0800

    add

---
+++ ZNRotateBannerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZNRotateBannerDemo
+//
+//  Created by 郑南 on 16/7/5.
+//  Copyright © 2016年 ZN. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ZNRotateBannerView.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
